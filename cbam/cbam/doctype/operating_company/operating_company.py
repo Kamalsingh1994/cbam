7cdde4f1aa6ad82c02bbadba0116ef07c3ba79ae
Nasir Khan <work.nasirkhan@gmail.com>
2024-12-17 07:05:13 +0000

    feat(Feature): Pages added

--- cbam/cbam/doctype/operating_company/operating_company.py
+++ cbam/cbam/doctype/operating_company/operating_company.py
@@ -7,6 +7,7 @@ from frappe.model.document import Document
 
 class OperatingCompany(Document):
 	def validate(self):
+		self.flags.new_flag = True
 		if self.create_commercial_contact_user and not self.commercial_contact_user:
 			username = frappe.db.get_value("User", self.main_contact_employee_email, "name")
 			if not username:
@@ -17,13 +18,31 @@ class OperatingCompany(Document):
 				user.append("roles",{
 					"role": frappe.db.get_single_value("CBAM Settings", "commercial_contact_user_role")
 				})
-				user.save()
+				user.save(ignore_permissions=True)
+				
 				username = user.name
 
 
 			self.commercial_contact_user = username
+		if not self.is_new():
+			self.flags.new_flag = False
+			self.create_permissions(username)
+
+	
+	def after_insert(self):
+		if self.flags.new_flag:
+			self.create_permissions()
 
 	@frappe.whitelist()
 	def send_signup_request(self):
 		email = frappe.get_doc("Notification", "Commercial Contact Signup Request")
 		email.send(self)
+
+	def create_permissions(self, user=None):
+		if self.commercial_contact_user and self.create_commercial_contact_user:
+			if not frappe.db.exists("User Permission", {"user": self.commercial_contact_user, "for_value":self.name}):
+				us_pem = frappe.new_doc("User Permission")
+				us_pem.user = user or self.commercial_contact_user
+				us_pem.allow = "Operating Company"
+				us_pem.for_value = self.name
+				us_pem.save(ignore_permissions=True)
