d50a25ae82856aace75d8be6141cd0ebdf916672
Nasir Khan <work.nasirkhan@gmail.com>
2025-03-18 20:30:14 +0000

    fix: supplier list restructured

--- cbam/www/supplier_list/index.py
+++ cbam/www/supplier_list/index.py
@@ -3,22 +3,8 @@ import frappe
 
 
 def get_context(context):
-    # context.user = frappe.session.user
-    # context.employee_list = frappe.db.get_all('Supplier Employee', filters={'email': context.user}, fields=['name'], pluck="name")
-    # if context.employee_list:
-    #     context.employee = context.employee_list[0]
-    #     context.goods_list = frappe.get_all('Good', 
-    #         filters={'employee': context.employee}, 
-    #         or_filters=[["status", "like", "Raw Data"], ["status", "like", "Sent for completing"], ["status", "like", "Done"]], 
-    #         fields=[
-    #             "*"
-    #         ]
-    #     )
-    # else:
-    #     context.goods_list = []
     context = get_suppliers(context)
-    return
-    context.supplier_list = frappe.db.get_all("Supplier")
+    return context
     
 @frappe.whitelist()
 def get_supplier_partial_html():
