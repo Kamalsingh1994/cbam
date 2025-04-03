63789600ed8a9c52e0a2a9fe2011ab31591d1c62
Kamal Singh <kamal.singh@phamos.eu>
2025-04-03 14:46:52 +0530

    revert the chnages of issue #305

--- cbam/hooks.py
+++ cbam/hooks.py
@@ -277,16 +277,7 @@ fixtures = [
                 "Declarant"
             ]
         ]
-    ]},
-    {"dt": "Dynamic Web Template", "filters": [
-        [
-            "name", "in", [
-                "Impressum-en",
-                "CBAM Home-en",
-                "Privacy Policy-en"
-            ]
-        ]
-    ]},
+    ]}
     # {"dt": "Custom DocPerm", "filters": [
     #     [
     #         "role", "in", [
