88ecf71697aa2376f30614528c9a0c19639d2bdd
Kamal Singh <kamal.singh@phamos.eu>
2025-06-13 11:57:15 +0530

    gallehr/cbam#477: update report and external good doctype

--- cbam/cbam/doctype/external_good/external_good.py
+++ cbam/cbam/doctype/external_good/external_good.py
@@ -6,12 +6,4 @@ from frappe.model.document import Document
 
 
 class ExternalGood(Document):
-	def validate(self):
-		if self.declarant and not self.is_user_allowed_declarant(self.declarant, frappe.session.user):
-			self.declarant = None
-	
-	def is_user_allowed_declarant(self, declarant, user):
-		return frappe.db.exists("Declarant User", {
-			"parent": declarant,
-			"user": user
-		})
+	pass
