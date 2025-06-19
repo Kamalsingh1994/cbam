b24fdac64640983537ff2a1df5d842a42fd8adb8
Kamal Singh <kamal.singh@phamos.eu>
2025-06-19 12:41:11 +0530

    gallehr/cbam#486: add Multi-Select Filters to Financial Evaluation Report

--- cbam/cbam/report/financial_evaluation/financial_evaluation.js
+++ cbam/cbam/report/financial_evaluation/financial_evaluation.js
@@ -2,7 +2,54 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Financial Evaluation"] = {
-	"filters": [
-
+	onload: function (report) {
+		const style = document.createElement('style');
+		style.innerHTML = `
+		  .dt-instance-1 .dt-cell__content--col-0 {
+			width: auto !important;
+		  }
+		`;
+		document.head.appendChild(style);
+	},	  
+	filters: [
+	  {
+		fieldname: "cn_code",
+		label: __("CN Code"),
+		fieldtype: "MultiSelectList",
+		get_data(txt) {
+		  return frappe.db.get_list("CN Code", {
+			fields: ["cn_code as value", "cn_code as description"],
+			filters: [["cn_code", "like", `%${txt}%`]],
+			limit: 20
+		  });
+		}
+	  },
+	  {
+		fieldname: "supplier",
+		label: __("Supplier"),
+		fieldtype: "MultiSelectList",
+		get_data(txt) {
+		  return frappe.db.get_list("Good", {
+			fields: ["supplier_name as value", "supplier_number as description"],
+			filters: [["supplier_name", "like", `%${txt}%`]],
+			distinct: true,
+			limit: 20
+		  });
+		}
+	  },
+	  {
+		fieldname: "article_number",
+		label: __("Article Number"),
+		fieldtype: "MultiSelectList",
+		get_data(txt) {
+		  return frappe.db.get_list("Good", {
+			fields: ["article_number as value" , "article_number as description"],
+			filters: [["article_number", "like", `%${txt}%`]],
+			distinct: true,
+			limit: 20
+		  });
+		}
+	  }
 	]
-};
+  };
+  
