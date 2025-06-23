176f243cab00304acc57e7659581c987436fade3
Kamal Singh <kamal.singh@phamos.eu>
2025-06-23 19:53:44 +0530

    gallehr/cbam#500: update table multiselct (Reporting Period)

--- cbam/cbam/doctype/reporting_period/reporting_period.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, phamos GmbH and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Reporting Period", {
-// 	refresh(frm) {
-
-// 	},
-// });
