3f3371e0d9685e8a636223630b790f7596e720be
Ulrich Fofe <ulrich.fofe@gallehr.de>
2026-01-19 17:33:04 +0100

    feat(#675):Add country code auto-fill, made modifications on the form accourding to #675

--- cbam/hooks.py
+++ cbam/hooks.py
@@ -276,24 +276,37 @@ override_whitelisted_methods = {
 # }
 
 fixtures = [
-    
-    {"dt": "Role", "filters": [
-        [
-            "name", "in", [
-                "Declarant",
-                "Commercial Contact",
-                "CBAM Representative"
+    {
+        "dt": "Role",
+        "filters": [
+            [
+                "name", "in", [
+                    "Declarant",
+                    "Commercial Contact",
+                    "CBAM Representative"
+                ]
             ]
         ]
-    ]
     },
-    {"dt": "Module Profile", "filters": [
-        [
-            "name", "in", [
-                "Declarant"
+    {
+        "dt": "Module Profile",
+        "filters": [
+            [
+                "name", "in", [
+                    "Declarant"
+                ]
             ]
         ]
-    ]}
+    },
+    "Custom Field",
+    "Property Setter",
+    "Client Script",
+    "Server Script",
+]
+
+permission_query_conditions = {
+    "User": "cbam.utils.utils.user_permission_query"
+}    
     # {"dt": "Custom DocPerm", "filters": [
     #     [
     #         "role", "in", [
@@ -314,8 +327,4 @@ fixtures = [
     #     ]
     # ]
     # },
-]
 
-permission_query_conditions = {
-    "User": "cbam.utils.utils.user_permission_query"
-}
